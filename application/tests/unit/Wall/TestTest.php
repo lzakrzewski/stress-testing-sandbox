fe5b45ae5266223b5451f530563dcbcf45493727
lzakrzewski <lucaszz111@gmail.com>
2016-10-15 22:10:54 +0100

    Project setup

---
+++ application/tests/unit/Wall/TestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace tests\unit\Wall;
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+    }
+}
