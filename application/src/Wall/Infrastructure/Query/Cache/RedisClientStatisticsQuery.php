3a49d4deea4d5a3edf149b99aa4b4a67f74a9bfb
lzakrzewski <lucaszz111@gmail.com>
2016-11-06 01:22:04 +0000

    RedisClientStatisticsQuery implementation

--- application/src/Wall/Infrastructure/Query/Cache/RedisClientStatisticsQuery.php
+++ application/src/Wall/Infrastructure/Query/Cache/RedisClientStatisticsQuery.php
@@ -4,13 +4,60 @@ declare(strict_types=1);
 
 namespace Wall\Infrastructure\Query\Cache;
 
+use Predis\Client as RedisClient;
 use Wall\Application\Query\ClientStatisticsQuery;
 use Wall\Application\Query\Result\ClientStatistics;
 
 class RedisClientStatisticsQuery implements ClientStatisticsQuery
 {
+    /** @var RedisClient */
+    private $redis;
+
+    public function __construct(RedisClient $redis)
+    {
+        $this->redis = $redis;
+    }
+
     public function get(): ClientStatistics
     {
-        return new ClientStatistics();
+        $mostOftenUsedBrowser = null;
+        $mostOftenUsedOS      = null;
+
+        $pipeline = $this->pipeline();
+
+        if (!empty($pipeline[0][0])) {
+            $mostOftenUsedBrowser = $pipeline[0][0];
+        }
+
+        if (!empty($pipeline[1][0])) {
+            $mostOftenUsedOS = $pipeline[1][0];
+        }
+
+        return new ClientStatistics($mostOftenUsedBrowser, $mostOftenUsedOS);
+    }
+
+    private function pipeline(): array
+    {
+        $pipeline = $this->redis->pipeline();
+
+        $pipeline->sort(
+            'browsers',
+            [
+                'by'    => 'browser_*',
+                'limit' => [0, 1],
+                'sort'  => 'desc',
+            ]
+        );
+
+        $pipeline->sort(
+            'os',
+            [
+                'by'    => 'os_*',
+                'limit' => [0, 1],
+                'sort'  => 'desc',
+            ]
+        );
+
+        return $pipeline->execute();
     }
 }
--- application/tests/integration/Wall/Infrastructure/Query/Cache/RedisClientStatisticsQueryTest.php
+++ application/tests/integration/Wall/Infrastructure/Query/Cache/RedisClientStatisticsQueryTest.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace tests\integration\Wall\Infrastructure\Query\Cache;
 
+use DeviceDetector\DeviceDetector;
+use Predis\Client as RedisClient;
+use Psr\Http\Message\ServerRequestInterface;
 use tests\integration\Wall\Infrastructure\CacheTestCase;
 use Wall\Application\Query\ClientStatisticsQuery;
 use Wall\Application\Query\Result\ClientStatistics;
+use Wall\Infrastructure\Query\Cache\RedisClientStatisticsProjector;
 use Wall\Infrastructure\Query\Cache\RedisClientStatisticsQuery;
+use Zend\Diactoros\ServerRequest;
 
 class RedisClientStatisticsQueryTest extends CacheTestCase
 {
@@ -22,6 +27,26 @@ class RedisClientStatisticsQueryTest extends CacheTestCase
         $this->assertEquals(new ClientStatistics(null, null), $statistics);
     }
 
+    /** @test */
+    public function it_returns_statistics()
+    {
+        $this->given(
+            $this->requestWithUserAgent(
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0'
+            ),
+            $this->requestWithUserAgent(
+                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
+            ),
+            $this->requestWithUserAgent(
+                'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'
+            )
+        );
+
+        $statistics = $this->query->get();
+
+        $this->assertEquals(new ClientStatistics('Firefox', 'Windows'), $statistics);
+    }
+
     protected function setUp()
     {
         parent::setUp();
@@ -34,5 +59,24 @@ class RedisClientStatisticsQueryTest extends CacheTestCase
         $this->query = null;
 
         parent::tearDown();
+    }
+
+    private function requestWithUserAgent(string $userAgent): ServerRequestInterface
+    {
+        return (new ServerRequest([], [], 'http://localhost/publish-post', 'POST'))
+            ->withHeader('user-agent', [$userAgent]);
+    }
+
+    private function given(...$requests)
+    {
+        foreach ($requests as $request) {
+            $projector = new RedisClientStatisticsProjector(
+                $this->container()->get(RedisClient::class),
+                $this->container()->get(DeviceDetector::class),
+                $request
+            );
+
+            $projector->applyThatPostWasPublished();
+        }
     }
 }
